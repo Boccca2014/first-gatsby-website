0123f30d8151aa68fe0f3e02f622d733d480f3e1
Sebastian Cabrejos <14218992+Boccca2014@users.noreply.github.com>
2023-03-02 14:37:47 -0500

    intermediary step - part 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 // Step 1: Import React
 import * as React from 'react'
+// import Layout from '../components/layout'
+import { Link } from 'gatsby' // TODO: premade React component, only use for internal links
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <main>
       <h1>Welcome to my Gatsby site!</h1>
+      <Link to="/about">About</Link>
       <p>I'm making this by following the Gatsby Tutorial.</p>
     </main>
   )
