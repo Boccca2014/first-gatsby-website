94a9616d13987634a7439130dbf9402c01829893
Sebastian Cabrejos <14218992+Boccca2014@users.noreply.github.com>
2023-03-05 08:33:56 -0500

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `First Gatsby Website`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
